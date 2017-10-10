d9e56f83c60ac32e3d028056ef605fded15f5879
Ryan Morrissey <me@rymo.io>
2017-10-09 23:20:48 -0600

    Remove incomplete files

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import (Flask, render_template, request, jsonify)
 from flask_restful import (Resource, Api)
 
-from .reports import report83, report85, report87, report88, report91
+from reports import report87, report88, report91
 
 
 app = Flask(__name__)
 api = Api(app)
-
-class Report83(Resource):
-    def get(self):
-        return jsonify(report83.get_data())
-
-class Report85(Resource):
-    def get(self):
-        return jsonify(report85.get_data())
 
 class Report87(Resource):
     def get(self):
@@ -27,8 +19,6 @@ class Report91(Resource):
     def get(self):
         return jsonify(report91.get_data())
 
-api.add_resource(Report83, '/api/report83')
-api.add_resource(Report85, '/api/report85')
 api.add_resource(Report87, '/api/report87')
 api.add_resource(Report88, '/api/report88')
 api.add_resource(Report91, '/api/report91')
--- reports/report85.py
+++
@@ -1,8 +0,0 @@
-from flask import (jsonify)
-from . import get_report_by_id
-
-json_data = get_report_by_id(85).json()
-value = json_data['value']
-
-def get_data():
-    return value
